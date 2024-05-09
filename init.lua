7ef497beb587dc3e6e4124144d4c83dc7ba6c54b
williammmm <91826947+williamwith4ms@users.noreply.github.com>
2024-05-09 22:46:01 +0100

    added keybinds and config

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 require("core.keymaps")
+require("core.config")
 require("core.plugins")
 require("core.plugin_config")
+
---
+++ lua/core/config.lua
@@ -0,0 +1,11 @@
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+
+-- spaces for tabs 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
