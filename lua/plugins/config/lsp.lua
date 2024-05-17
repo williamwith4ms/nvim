37de04e1adad9fa0711d8d764ba05dff58c6be97
williammmm <91826947+williamwith4ms@users.noreply.github.com>
2024-05-17 18:55:30 +0100

    auto complete

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -1,23 +1,30 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls","rust_analyzer","pylsp" }
+	ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "tsserver" },
 })
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 local on_attach = function(_, _)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+	capabilities = capabilities
 end
 
+require("lspconfig").lua_ls.setup({
+	on_attach = on_attach,
+})
 
-require("lspconfig").lua_ls.setup {
-  on_attach = on_attach,
-}
+require("lspconfig").rust_analyzer.setup({
+	on_attach = on_attach,
+})
 
-require("lspconfig").rust_analyzer.setup {
-  on_attach = on_attach,
-}
+require("lspconfig").pylsp.setup({
+	on_attach = on_attach,
+})
 
-require("lspconfig").pylsp.setup {
+require("lspconfig").tsserver.setup({
   on_attach = on_attach,
-}
+})
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,38 +1,46 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  -- Themes
-  "navarasu/onedark.nvim",
-  "nvim-lualine/lualine.nvim",
-  "nvim-tree/nvim-web-devicons",
-  "goolord/alpha-nvim",
-  -- LSP
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  "nvim-telescope/telescope-ui-select.nvim",
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = { "nvimtools/none-ls-extras.nvim" },
-  },
-  -- Features
-  "nvim-tree/nvim-tree.lua",
-  "nvim-treesitter/nvim-treesitter",
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
+	-- Themes
+	"navarasu/onedark.nvim",
+	"nvim-lualine/lualine.nvim",
+	"nvim-tree/nvim-web-devicons",
+	"goolord/alpha-nvim",
+	-- LSP
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"nvim-telescope/telescope-ui-select.nvim",
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = { "nvimtools/none-ls-extras.nvim" },
+	},
+	-- Features
+	"nvim-tree/nvim-tree.lua",
+	"nvim-treesitter/nvim-treesitter",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	-- autocomplete
+	"hrsh7th/nvim-cmp",
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	"saadparwaiz1/cmp_luasnip",
+  "hrsh7th/cmp-nvim-lsp",
 }
 
 local opts = {}
