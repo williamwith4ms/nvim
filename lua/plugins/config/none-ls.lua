c842c74b534d4a87ef23dbeb36a932eb341fcb72
williammmm <91826947+williamwith4ms@users.noreply.github.com>
2024-05-17 17:41:33 +0100

    eslint

--- lua/plugins/config/none-ls.lua
+++ lua/plugins/config/none-ls.lua
@@ -3,6 +3,12 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.prettier,
+
+    require("none-ls.diagnostics.eslint_d").with({ condition = function(utils) return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs" }) end, }),
+
 	},
 })
 
--- lua/plugins/config/nvim-treesitter.lua
+++ lua/plugins/config/nvim-treesitter.lua
@@ -1,5 +1,4 @@
 require'nvim-treesitter.configs'.setup {
-
   ensure_installed = { "c", "lua", "rust", "python", "javascript" },
   sync_install = false,
   auto_install = true,
