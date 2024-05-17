1a4922466d0fd6b2c2a9270c747412ae64fd0072
williammmm <91826947+williamwith4ms@users.noreply.github.com>
2024-05-17 18:20:31 +0100

    alpha

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,37 +1,38 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	-- Themes
-	"navarasu/onedark.nvim",
-	"nvim-lualine/lualine.nvim",
-	"nvim-tree/nvim-web-devicons",
-	-- LSP
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
-	"nvim-telescope/telescope-ui-select.nvim",
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = { "nvimtools/none-ls-extras.nvim" },
-	},
-	-- Features
-	"nvim-tree/nvim-tree.lua",
-	"nvim-treesitter/nvim-treesitter",
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+  -- Themes
+  "navarasu/onedark.nvim",
+  "nvim-lualine/lualine.nvim",
+  "nvim-tree/nvim-web-devicons",
+  "goolord/alpha-nvim",
+  -- LSP
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  "nvim-telescope/telescope-ui-select.nvim",
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvimtools/none-ls-extras.nvim" },
+  },
+  -- Features
+  "nvim-tree/nvim-tree.lua",
+  "nvim-treesitter/nvim-treesitter",
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 }
 
 local opts = {}
