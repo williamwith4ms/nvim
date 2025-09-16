c9f3be1f317625554a76fcfab4f4b62cd4ee8b7a
williammmm <me@wiliammmm.dev>
2025-09-16 01:25:11 +0100

    fix disable in tex

--- lua/plugins/config/copilot.lua
+++ lua/plugins/config/copilot.lua
@@ -1,3 +1,7 @@
-vim.g.copilot_filetypes = {
-  tex = false,
-}
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "tex", "plaintex" },
+  callback = function()
+    vim.b.copilot_enabled = false
+  end,
+})
+
